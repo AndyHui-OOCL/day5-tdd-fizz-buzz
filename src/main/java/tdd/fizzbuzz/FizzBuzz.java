2ebb9a9080ffe8e89dd8167249e6a9263d2e3919
Andy Hui <andy.hui@oocl.com>
2025-09-05 11:41:41 +0800

    feat: implement FizzBuzz countOff multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     public String countOff(int input) {
         if(input % 3 == 0) {
             return "Fizz";
+        } else if(input % 5 == 0) {
+            return "Buzz";
         }
         return String.valueOf(input);
     }
