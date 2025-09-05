c884e23d24710006b080060cc93d61c9f2b06ca6
Andy Hui <andy.hui@oocl.com>
2025-09-05 11:36:15 +0800

    test: normal number for FizzBuzz countOff

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_normal_number_when_countOff_given_normal_number(){
+        // Given
+        int input = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // When
+        String result = fizzBuzz.countOff(input);
+
+        assertEquals("1", result);
+    }
 }
