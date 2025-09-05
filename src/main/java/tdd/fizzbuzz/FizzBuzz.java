33cd347369969fa7d3554269deb931c967bc444d
Andy Hui <andy.hui@oocl.com>
2025-09-05 11:45:03 +0800

    feat: implement FizzBuzz countOff multiple of 15

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,10 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int input) {
-        if(input % 3 == 0) {
+        if(input % 3 == 0 && input % 5 == 0){
+            return "FizzBuzz";
+        }
+        else if(input % 3 == 0) {
             return "Fizz";
         } else if(input % 5 == 0) {
             return "Buzz";
